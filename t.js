eedec468d730160f3c64af97421c5567a3d7e705
xubo-bj <xubo_bj@qq.com>
2017-07-04 14:41:49 +0800

    '1441'

--- t.js
+++ t.js
@@ -13,17 +13,15 @@ parseString(str, function (err, result) {
 });
 
 https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx40d6bb4bd6340273&redirect_uri=http://www.xubo.ren/auth&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect
-
-https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx40d6bb4bd6340273&secret=eff808be65f47862a8d98e321218fde3&code=031T1aed2peX5C0IT5ed2oFsed2T1aeJ&grant_type=authorization_code 
+f9f53fb4ae92f7ab1facd3a2ff1af4fa23f6996d
+f9f53fb4ae92f7ab1facd3a2ff1af4fa23f6996d
 
 
 获取第二步的refresh_token后，请求以下链接获取access_token：  
 
 https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=APPID&grant_type=refresh_token&refresh_token=REFRESH_TOKEN 
 
-09d229843abe82ee87efdc61c9b383cda5123457
-09d229843abe82ee87efdc61c9b383cda5123457
-
+f4e_EP4z--62o2twCLCMkDhBh1DCTFhLAb9L8S6a-At_guZFLnRgCzU59arzzAISoE95MtsUSs5EI7TVBPqW31TUQaYPXix4uSuYUwu6go815Os2MTn5Ci8MebAJXfipTYUcAGAOTP
 
 jsapi_ticket=kgt8ON7yVITDhtdwci0qeR_Uf9bxN_DpMxWLSd6uzFYZzVdXK1BY74kIqCxBsT7N4I5_j17_bnrqiITjByQ9Nw&noncestr=kgt8ON7yVITDhtdwci0qeR_Uf9bxN_DpMxWLSd6uzFYZzVdXK1BY74kIqCxBsT7N4I5_j17_bnrqiITjByQ9Nw&timestamp=1499044757&url=http://www.xubo.ren/auth/
 jsapi_ticket=kgt8ON7yVITDhtdwci0qeR_Uf9bxN_DpMxWLSd6uzFYZzVdXK1BY74kIqCxBsT7N4I5_j17_bnrqiITjByQ9Nw&noncestr=Wm3WZYTPz0wzccnW&timestamp=1499044757&url=http://www.xubo.ren/auth/
